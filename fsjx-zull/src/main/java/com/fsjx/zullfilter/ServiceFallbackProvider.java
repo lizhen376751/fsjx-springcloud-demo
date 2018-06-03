a45a8e3ff75d6d207e32b5d71816a7bd82d3afc8
724577807@qq.com <724577807@qq.com>
2018-06-04 01:55:36 +0800

    zull的熔断

---
+++ fsjx-zull/src/main/java/com/fsjx/zullfilter/ServiceFallbackProvider.java
@@ -0,0 +1,63 @@
+package com.fsjx.zullfilter;
+
+import org.springframework.cloud.netflix.zuul.filters.route.ZuulFallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+/**
+ * zull的熔断机制
+ * Created by lizhen on 2018/6/4.
+ */
+@Component
+public class ServiceFallbackProvider implements ZuulFallbackProvider {
+    @Override
+    public String getRoute() {
+        return "*";
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse() {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return this.getStatusCode().value();
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return this.getStatusCode().getReasonPhrase();
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream("服务暂时不可用".getBytes());
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                MediaType mt = new MediaType("application", "json", Charset.forName("UTF-8"));
+                headers.setContentType(mt);
+                return headers;
+            }
+        };
+    }
+}
